05ff39409a5b6734674ac207132188d69eb35bd1
prabhavsharma <ps2921@nyu.edu>
2016-04-11 18:55:19 -0400

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Team 9</div>
+                <div class="title"> hello Team 9</div>
             </div>
         </div>
     </body>
