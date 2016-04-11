4aec080f77c797aaa9f42bfe2e03f759db772ccb
arogozin <aleksandr@rogozin.us>
2016-04-10 20:38:33 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hackathon Team 9</div>
+                <div class="title">Team 9</div>
             </div>
         </div>
     </body>
